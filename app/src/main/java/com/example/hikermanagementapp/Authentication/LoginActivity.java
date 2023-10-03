c7f1c6237b8bce9cddc5d8160a7b2d4b7f972302
Harry Tran <trungtien2811@gmail.com>
2023-10-03 07:34:42 +0700

    Initial commit

---
+++ app/src/main/java/com/example/hikermanagementapp/Authentication/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.hikermanagementapp.Authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hikermanagementapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
